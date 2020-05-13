3887473463201b8c072693963039eeb20757ae53
Lily Xu <lilyleexu@gmail.com>
2020-05-13 19:00:07 -0400

    code to visualize examples of random graph for figure

---
+++ FinalP/visualize_graphs.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# gnp graph
+n = 8
+p = 0.3
+gnp = nx.gnp_random_graph(n, p)
+nx.draw_networkx(gnp)
+plt.show()
+
+# gnm graph
+m = 16
+gnm = nx.gnm_random_graph(n, m)
+nx.draw_networkx(gnm)
+plt.show()
+
+# grid graph
+grid = nx.grid_2d_graph(2, 4)
+nx.draw(grid)
+plt.show()
